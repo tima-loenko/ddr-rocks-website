457b566b834247b666d855c04f393520315eb22b
tima-loenko <tima.loenko@gmail.com>
2026-09-08 16:31:38 +0500

    custom deployment

--- astro.config.ts
+++ astro.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // used to generate images
-  site: 'https://tima-loenko.github.io',
-  base: '/ddr-rocks-website', // change to custom https://docs.astro.build/en/guides/deploy/github/#change-your-github-url-to-a-custom-domain
+  site: 'https://ddr.rocks',
+  base: '/', // change to custom https://docs.astro.build/en/guides/deploy/github/#change-your-github-url-to-a-custom-domain
   trailingSlash: 'ignore',
   integrations: [sitemap()],
   vite: {
